7dda88e4c6aefbbe799a1d4635eda50965f4a61c
HidayatRf <rezafairuzh54@gmail.com>
2023-03-19 10:20:56 +0700

    add Readme file and finish the responsive until 1280px

---
+++ Readme.md
@@ -0,0 +1,10 @@
+# d"SaatSenjaCoffee
+
+### hi i'am reza fairuz
+### i'am 17 years old
+
+this time i build a web design for coffee shop
+and for increase my skill 
+i build this website using a mobile phone, and i try hard
+for build a responsive section. maybe the results will be a little strange, 
+I'm sorry about that, and I will try to make the results match my expectations
--- main.js
+++ main.js
@@ -4,12 +4,17 @@ const navbar = document.querySelector('.navbar');
 const navLink = navbar.querySelector('.navLink');
 const openNavLink = navbar.querySelector('.openNavLink')
 
-openNavLink.addEventListener('click', () => {
+openNavLink.addEventListener('click', function() {
 	navLink.classList.toggle('active')
 })
 
 
+const contactUs = document.querySelector(".contact-us")
+const inputMessage = contactUs.querySelector(".wrapper .input-message")
+const sendMessage = contactUs.querySelector(".wrapper .send-message")
+
 window.addEventListener('load', () => {
 	container.style.paddingTop = `${navbar.clientHeight}px`
 	navLink.style.top = `${navbar.clientHeight}px`
+	inputMessage.innerHTML = ""
 })
